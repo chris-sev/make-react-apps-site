fb27d6cf22945d55ef9d4c52a452f317286a1f9f
Chris on Code <chris@scotch.io>
2020-04-15 08:51:48 -0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default function Footer() {
-  return <div>footer</div>
+  return (
+    <div className="bg-yellow-400 text-yellow-800 text-center text-xs p-2">
+      CodeRight © CopyWrong ©{" "}
+      <a
+        href="https://twitter.com/chrisoncode"
+        className="underline hover:text-yellow-700"
+      >
+        Chris on Code
+      </a>{" "}
+      1756
+    </div>
+  )
 }
