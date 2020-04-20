4321f74fb246141bfbcb1e63d24618bcc8f4f62c
Chris on Code <chris@scotch.io>
2020-04-20 07:58:12 -0700

    dabbing favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
+        icon: `src/images/dab.png`,
       },
     },
     {
