f0dca87062c15ad538472577c57a6be3ae167427
Chris on Code <chris@scotch.io>
2020-06-15 16:09:35 -0700

    adding login button

--- src/components/header.js
+++ src/components/header.js
@@ -2,52 +2,33 @@ import React from "react"
 
 export default function Header() {
   return (
-    <header className="text-right justify-end items-center pt-5 px-2 text-blue-900 text-sm">
+    <header className="text-right flex justify-end items-center pt-5 px-2 text-blue-900 text-xs">
       <div className="mr-4">
-        A{" "}
-        <span
-          className="inline-block text-yellow-400"
-          style={{ transform: "rotate(-27deg)" }}
-        >
-          fun
-        </span>{" "}
-        <span
-          className="inline-block text-red-600"
-          style={{ transform: "rotate(-67deg)" }}
-        >
-          video
-        </span>{" "}
-        <span
-          className="inline-block text-blue-600"
-          style={{ transform: "rotate(-17deg)" }}
-        >
-          course
-        </span>{" "}
-        by{" "}
+        By{" "}
         <a
           href="https://twitter.com/chrisoncode"
-          className="text-blue-800 underline hover:text-blue-100 hover:underline"
+          className="text-blue-700 hover:text-blue-100 hover:underline"
         >
           @ChrisOnCode
         </a>
         , the creator of{" "}
         <a
           href="https://scotch.io"
-          className="text-blue-800 underline hover:text-blue-100 hover:underline"
+          className="text-blue-700 hover:text-blue-100 hover:underline"
         >
           Scotch.io
         </a>
       </div>
       {/* <a href="https://learn.chrisoncode.io/">Purchase</a> */}
-      {/* <div className="bg-blue-300 text-blue-600 py-2 px-3 rounded-md flex items-center">
-        <span>Already purchased?</span>
+      <div className="bg-blue-300 text-blue-600 py-1 px-1 rounded-md flex items-center">
+        <span className="pl-2">Already purchased?</span>
         <a
           href="https://learn.chrisoncode.io/"
           className="ml-3 bg-blue-700 text-blue-200 rounded shadow py-2 px-3 hover:bg-blue-600"
         >
           Login
         </a>
-      </div> */}
+      </div>
     </header>
   )
 }
