5118d0d1a8ef784a5c7f712904e424dcb39b8030
Chris on Code <chris@scotch.io>
2020-06-16 15:29:52 -0700

    podia plug

--- src/components/usePodia.js
+++ src/components/usePodia.js
@@ -1,5 +1,12 @@
 import { useEffect } from "react"
 
+/**
+ * Podia hosts my courses.
+ * I love their platform because its everything under 1 roof and SUPER affordable compared to the competitors.
+ * Here's a referral code if you want to try them out:
+ * https://www.podia.com/?via=chris-on-code
+ */
+
 export default function usePodia() {
   useEffect(() => {
     if (document.getElementById("podia-script")) return
