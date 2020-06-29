eb00fbcb8afde340721bfbdb6f111c8c4b3e77cb
Chris on Code <chris@scotch.io>
2020-06-29 06:36:39 -0700

    adding second pricing option

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -11,62 +11,50 @@ export default function Pricing() {
       id="pricing"
       className="bg-green-600 text-gray-300 py-32 px-6 lg:px-24 lg:pt-32 lg:pb-40"
     >
-      <div className="container mx-auto flex justify-around">
-        <div className="w-full lg:w-2/3 xl:w-1/3 mr-10">
+      <div className="container mx-auto md:flex justify-center">
+        <div className="w-full lg:w-2/3 xl:w-1/3 flex items-center">
           {/* pricing boxes for 1-10 / 11-20 ==================================================== */}
-          <div className="bg-white mb-6 rounded shadow-lg text-gray-900">
-            <h3 className="fugaz-one bg-gray-200 w-full pt-6 pb-4 lg:pt-8 lg:pb-6 px-4 text-center text-xl text-gray-500 font-bold uppercase rounded-t-lg">
-              Buy Beginner Apps{" "}
-              <strong className="text-gray-700 ml-2">(1-10)</strong>
+          <div className="bg-white mb-6 rounded-lg md:rounded-l-lg md:rounded-r-none shadow-lg text-gray-900 w-full">
+            <h3 className="fugaz-one bg-gray-200 w-full pt-6 pb-4 lg:pt-8 lg:pb-6 px-4 text-center text-xl text-gray-500 font-bold uppercase rounded-t-lg md:rounded-tl-lg md:rounded-tr-none">
+              Starter Apps
             </h3>
             <div className="pt-6 pb-8 px-6 lg:px-10">
               {/* price info */}
               <div className="mb-6 text-gray-700 text-xl md:text-3xl flex items-center justify-center leading-none">
-                <div className="has-dollar-sign flex items-center text-6xl mx-8 pb-2">
-                  <span className="text-3xl mr-2">$</span>
-                  <span>44</span>
+                <div className="has-dollar-sign flex items-center text-5xl mx-3 pb-2">
+                  <span className="text-2xl mr-3">$</span>
+                  <span>49</span>
                 </div>
-                <div className="flex items-center text-gray-600">USD</div>
+                <div className="flex items-center text-gray-600 text-xl">
+                  USD
+                </div>
               </div>
 
               {/* main info */}
+              <p className="leading-loose mb-8 lg:text-xl font-normal opacity-75">
+                <span role="img" aria-label="Check">
+                  ✔
+                </span>{" "}
+                <strong>The first 10 React Apps</strong>
+                <br />
+                <span role="img" aria-label="Check">
+                  ✔
+                </span>{" "}
+                Code for the apps
+                <br />
+                <span role="img" aria-label="Check">
+                  ✔
+                </span>{" "}
+                Unlimited updates
+              </p>
               <a
-                href="https://learn.chrisoncode.io/make-20-react-apps"
+                href="https://learn.chrisoncode.io/make-20-react-apps-starter"
                 data-podia-embed="link"
-                className="checkout-button flex items-center justify-between xl:text-lg bg-gray-400 text-gray-700 shadow rounded-lg px-6 py-4 cursor-pointer hover:bg-gray-300 w-full transition-colors mb-2"
+                className="checkout-button flex items-center justify-between xl:text-lg bg-blue-300 text-blue-700 shadow rounded px-6 py-4 cursor-pointer hover:bg-blue-300 w-full transition-colors mb-2"
               >
-                <div>Get Apps 1-10</div>
+                <div>Get apps 1-10</div>
                 <div className="flex items-center">
-                  <span className="text-gray-900">$39</span>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="bg-white rounded shadow-lg text-gray-900">
-            <h3 className="fugaz-one bg-gray-200 w-full pt-6 pb-4 lg:pt-8 lg:pb-6 px-4 text-center text-xl text-gray-500 font-bold uppercase rounded-t-lg">
-              Buy Intermediate Apps{" "}
-              <strong className="text-gray-700 ml-2">(11-20)</strong>
-            </h3>
-            <div className="pt-6 pb-8 px-6 lg:px-10">
-              {/* price info */}
-              <div className="mb-6 text-gray-700 text-xl md:text-3xl flex items-center justify-center leading-none">
-                <div className="has-dollar-sign flex items-center text-6xl mx-8 pb-2">
-                  <span className="text-3xl mr-2">$</span>
-                  <span>44</span>
-                </div>
-                <div className="flex items-center text-gray-600">USD</div>
-              </div>
-
-              {/* main info */}
-              <a
-                href="https://learn.chrisoncode.io/make-20-react-apps"
-                data-podia-embed="link"
-                className="checkout-button flex items-center justify-between xl:text-lg bg-gray-400 text-gray-700 shadow rounded-lg px-6 py-4 cursor-pointer hover:bg-gray-300 w-full transition-colors"
-              >
-                <div>Get Apps 11-20</div>
-                <div className="flex items-center">
-                  <span className="text-gray-900">$49</span>
+                  <span className="text-blue-800">$49</span>
                 </div>
               </a>
             </div>
@@ -74,9 +62,9 @@ export default function Pricing() {
         </div>
 
         {/* pricing box ==================================================== */}
-        <div className="bg-white rounded-lg shadow-lg w-full lg:w-2/3 xl:w-1/2 mr-auto text-gray-900">
+        <div className="bg-white rounded-lg shadow-lg w-full lg:w-2/3 xl:w-2/5 text-gray-900">
           <h3 className="fugaz-one bg-green-200 w-full pt-6 pb-4 lg:pt-8 lg:pb-6 px-4 text-center text-2xl lg:text-4xl text-green-400 font-bold uppercase rounded-t-lg">
-            Buy All <strong className="text-green-700">20 Apps</strong>
+            <strong className="text-green-700">Complete</strong> Package
           </h3>
 
           <div className="pt-10 pb-12 px-6 lg:px-16">
@@ -118,15 +106,10 @@ export default function Pricing() {
             <a
               href="https://learn.chrisoncode.io/make-20-react-apps"
               data-podia-embed="link"
-              className="checkout-button flex items-center justify-between xl:text-2xl bg-yellow-400 text-yellow-900 shadow-lg rounded-lg px-6 py-6 cursor-pointer hover:bg-yellow-300 w-full transition-colors"
+              className="checkout-button flex items-center justify-between xl:text-2xl bg-yellow-400 text-yellow-900 shadow rounded px-6 py-6 cursor-pointer hover:bg-yellow-400 w-full transition-colors transition-transform duration-150"
             >
               <div>Get all 20 apps</div>
-              <div className="flex items-center">
-                <span className="line-through text-red-500 mr-3 text-base opacity-50">
-                  $130
-                </span>
-                <span className="text-yellow-900">$79</span>
-              </div>
+              <span className="text-yellow-900">$79</span>
             </a>
 
             <Countdown />
