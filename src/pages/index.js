f91780a5d9d49d3f1eb7efa1929db0aafaca6de3
Chris on Code <chris@scotch.io>
2020-07-02 15:37:56 -0700

    blahhhh

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,14 +27,14 @@ export default function IndexPage() {
       <SEO title="Make React Apps - Practical React Learning" />
 
       {/* hero section */}
-      <div className="hero-hero text-blue-100 min-h-screen p-10 flex items-center justify-center text-center relative">
+      <div className="hero-hero text-blue-100 min-h-screen p-10 flex items-center relative">
         {/* floaty logo */}
-        <div className="icons">
+        {/* <div className="icons">
           <ReactIcon />
           <ReactIcon />
           <ReactIcon />
           <ReactIcon />
-        </div>
+        </div> */}
 
         {/* content */}
         <div className="relative z-10">
@@ -45,7 +45,7 @@ export default function IndexPage() {
             Resources and courses to create real-world React projects and
             experiences.
           </p>
-          <div className="flex justify-center">
+          <div className="flex">
             <button className="inline-block text-md py-3 px-8 rounded shadow mr-4 bg-yellow-400 text-yellow-900">
               View Resources
             </button>
@@ -57,7 +57,7 @@ export default function IndexPage() {
       </div>
 
       {/* courses */}
-      <div>courses section</div>
+      <Courses />
 
       {/* free webinars */}
       <div>free webinars</div>
@@ -65,5 +65,89 @@ export default function IndexPage() {
       {/* free resources */}
       <div>free resources</div>
     </Layout>
+  )
+}
+
+const courses = [
+  {
+    url: "",
+    image_url:
+      "https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1588885521/sb9rkn08jtfswrpxc2uu.png",
+    title: "Getting Started with React",
+    description: "",
+    price: 59,
+  },
+  {
+    url: "",
+    image_url:
+      "https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1577202852/ykxvscombfo4iilkapmj.png",
+    title: "10 React Apps (Series 1)",
+    description: "",
+    price: 59,
+  },
+  {
+    url: "",
+    image_url:
+      "https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1588026018/plofnxmvzkowzvisi1za.png",
+    title: "10 React Apps (Series 2)",
+    description: "",
+    price: 59,
+  },
+  ,
+  {
+    url: "",
+    image_url:
+      "https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1588026018/plofnxmvzkowzvisi1za.png",
+    title: "10 React Apps (Series 2)",
+    description: "",
+    price: 59,
+  },
+  ,
+  {
+    url: "",
+    image_url:
+      "https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1588026018/plofnxmvzkowzvisi1za.png",
+    title: "10 React Apps (Series 2)",
+    description: "",
+    price: 59,
+  },
+  ,
+  {
+    url: "",
+    image_url:
+      "https://scotch-res.cloudinary.com/image/upload/w_1050,q_auto:good,f_auto/v1588026018/plofnxmvzkowzvisi1za.png",
+    title: "10 React Apps (Series 2)",
+    description: "",
+    price: 59,
+  },
+]
+
+function Courses() {
+  return (
+    <div className="course-section text-purple-100 py-32">
+      <div className="container mx-auto">
+        {/* the header  */}
+        <div className="text-center mb-10">
+          <h2 className="text-4xl">React Courses</h2>
+          <h3>Videos to code along with</h3>
+        </div>
+
+        {/* loop over the courses */}
+        <div className="super-grid grid grid-cols-3 gap-4">
+          {courses.map((course, index) => (
+            <div key={index} className="bg-white rounded shadow">
+              <img
+                className="h-full w-full rounded object-cover"
+                src={course.image_url}
+                alt={course.title}
+              />
+              {/* <div className="p-1 text-gray-700">
+                <p className="text-xs my-0">{course.title}</p>
+              </div> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
