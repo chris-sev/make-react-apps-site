0d9a684ce41cde2d0d74a887b4f793a066f2667a
Chris on Code <chris@scotch.io>
2020-07-14 22:31:56 -0700

    footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,31 +2,33 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-blue-200 text-blue-800 text-center text-xs p-5">
-      <div className="mb-3">
-        <a
-          href="https://github.com/chris-on-code/make-react-apps-site"
-          className="hover:text-blue-700 text-blue-900"
-        >
-          View the <span className="underline">GitHub Repo</span> and see how I
-          built this site.
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://www.podia.com/?via=chris-on-code"
-          className="underline text-blue-900 hover:text-blue-800 mr-2"
-        >
-          Powered by Podia
-        </a>
-        CodeRight © CopyWrong ©{' '}
-        <a
-          href="https://twitter.com/chrisoncode"
-          className="underline text-blue-900 hover:text-blue-800"
-        >
-          Chris on Code
-        </a>{' '}
-        1756
+    <div className="bg-blue-900 text-blue-200 text-center text-xs p-5">
+      <div className="container mx-auto lg:flex justify-between">
+        <div className="mb-3 lg:mb-0">
+          <a
+            href="https://github.com/chris-on-code/make-react-apps-site"
+            className="hover:text-blue-100 text-blue-300"
+          >
+            View the <span className="underline">GitHub Repo</span> and see how
+            I built this site.
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.podia.com/?via=chris-on-code"
+            className="underline hover:text-blue-100 text-blue-300 mr-2"
+          >
+            Powered by Podia
+          </a>
+          CodeRight © CopyWrong ©{' '}
+          <a
+            href="https://twitter.com/chrisoncode"
+            className="underline hover:text-blue-100 text-blue-300"
+          >
+            Chris on Code
+          </a>{' '}
+          1756
+        </div>
       </div>
     </div>
   )
