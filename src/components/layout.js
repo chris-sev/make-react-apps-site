ff431cc8e0b16cff91e17288b31307df30f032bf
Chris Sev <chris.sevilleja@gmail.com>
2021-07-12 13:10:20 -0700

    go go

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
-import React from "react"
-import PropTypes from "prop-types"
-import "./fontawesome"
-import "./layout.css"
+import React from 'react'
+import PropTypes from 'prop-types'
+import './layout.css'
 
 export default function Layout({ children }) {
   return (
